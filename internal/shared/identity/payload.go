baaded8977dcce867354e4e4d872e9df2a91245e
edward ciccone <ciccone.etp@gmail.com>
2026-01-24 23:02:49 -0600

    chore: added IdentityRequestEvtPayload

--- internal/shared/identity/payload.go
+++ internal/shared/identity/payload.go
@@ -17,3 +17,20 @@ func (p *IdentityRequestCmdPayload) Validate() error {
 
 	return nil
 }
+
+type IdentityRequestEvtPayload struct {
+	WithdrawalID string  `json:"withdrawal_id"`
+	UserID       string  `json:"user_id"`
+	Reason       *string `json:"reason,omitempty"`
+}
+
+func (p *IdentityRequestEvtPayload) Validate() error {
+	if p.WithdrawalID == "" {
+		return errors.New("withdrawal_id is empty")
+	}
+	if p.UserID == "" {
+		return errors.New("user_id is empty")
+	}
+
+	return nil
+}
